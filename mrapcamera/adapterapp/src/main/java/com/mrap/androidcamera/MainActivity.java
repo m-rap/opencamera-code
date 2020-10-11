28fbc73efbf5f96aa668252f8463587973393082
rian <mohammad.rian.ap@gmail.com>
2020-10-11 17:25:01 +0700

    adapter from android-camera repo moved here

---
+++ mrapcamera/adapterapp/src/main/java/com/mrap/androidcamera/MainActivity.java
@@ -0,0 +1,30 @@
+package com.mrap.androidcamera;
+
+//import android.app.Activity;
+//import android.os.Bundle;
+
+public class MainActivity extends net.sourceforge.opencamera.MainActivity {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+//
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//    }
+}
